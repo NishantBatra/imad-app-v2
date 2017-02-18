a98ec62c5aae184a5514392f4be697f187df3c28
NishantBatra <nbat07rkr@gmail.com>
2017-02-18 19:59:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/cr7.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'cr7.jpg'));
 });
 
